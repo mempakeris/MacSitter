0c24ddbd919e0051f898252cb0fab9baed514649
caliskimmer <caliskimmer@gmail.com>
2017-12-31 00:10:30 -0800

    added reference to external lib used for some features and updated lib with fan speed functions

--- sysinfo.swift
+++ sysinfo.swift
@@ -11,7 +11,8 @@ import Foundation
 
 public class SysInfo {
     
-    /***PUBLIC METHODS***/
+    // MARK: Public Methods
+    
     public static func totalHddSpace() -> Float {
         guard
             let fileSystemAttributeDict = try? homeFileSystemAttributeDict()
@@ -40,7 +41,59 @@ public class SysInfo {
         return freeSpace.floatValue / gigabyteDivider
     }
     
-    /*** PRIVATE METHODS ***/
+    public static func cpuTemperature(_ temperatureUnit:TemperatureUnit) -> Double {
+        var temperature : Double = 0.0
+        do {
+            try SMCKit.open()
+            temperature = try SMCKit.temperature(TemperatureSensors.CPU_0_DIE.code, unit:temperatureUnit)
+            SMCKit.close()
+        } catch {
+            print(error)
+            SMCKit.close()
+        }
+        
+        return temperature
+    }
+    
+    public static func currentFanSpeeds() -> [Int] {
+        var fanSpeeds:[Int] = []
+        do {
+            try SMCKit.open()
+            
+            let numFans = try SMCKit.fanCount()
+            for ii in 0...numFans-1 {
+                fanSpeeds.append(try SMCKit.fanCurrentSpeed(ii))
+            }
+            
+            SMCKit.close()
+        } catch {
+            print(error)
+            SMCKit.close()
+        }
+        
+        return fanSpeeds
+    }
+    
+    func maxFanSpeeds() -> [Int] {
+        var fanSpeeds:[Int] = []
+        do {
+            try SMCKit.open()
+            
+            let numFans = try SMCKit.fanCount()
+            for ii in 0...numFans-1 {
+                fanSpeeds.append(try SMCKit.fanMaxSpeed(ii))
+            }
+            
+            SMCKit.close()
+        } catch {
+            print(error)
+            SMCKit.close()
+        }
+        
+        return fanSpeeds
+    }
+    
+    // MARK: Private Methods
     
     private static func homeFileSystemAttributeDict() throws -> [FileAttributeKey : Any] {
         var fileSystemAttributeDict = [FileAttributeKey : Any]()
